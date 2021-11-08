69b52831d9ea5c163bb6645e366cd3b06416f5d4
Pasit Sri-intarasut <s6301012620065@kmutnb.ac.th>
2021-11-08 11:41:07 +0700

    feat: create searchDirectory method

--- Directory.java
+++ Directory.java
@@ -3,8 +3,13 @@ class Directory{
 	Directory(String path){
 		this.path = path;
 	}
+
+	File[] searchDirectory(String path){
+		File f = new File(path);
+		return f.listFile();
+	}
 	
-	Boolean isFile(String path){
+	boolean isFile(String path){
 		File f = new File(path);
 		return !f.isDirectory();
 	}
