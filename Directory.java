b69d95efdcdc9c0d0a7efaafd1cb91ac2e67fc52
Pasit Sri-intarasut <s6301012620065@kmutnb.ac.th>
2021-11-08 11:48:02 +0700

    feat: add showDirectory method

--- Directory.java
+++ Directory.java
@@ -1,16 +1,39 @@
+import java.io.*;
+import java.util.*;
+
 class Directory{
 	String path = "";
+
+
+	public static void main(String[] args){
+		Directory d = new Directory("/home/dsypasit/code/university/DirecTest/test");
+		d.showDirectory();
+	}
+
+
 	Directory(String path){
 		this.path = path;
 	}
 
+	void showDirectory(){
+		File f = new File(path);
+		File[] child = searchDirectory(path);
+		System.out.println(f.getName());
+		for(File c: child){
+			if(!isFile(c)){
+				System.out.println("+"+c.getName());
+			}else{
+				System.out.println("-"+c.getName());
+			}
+		}
+	}
+
 	File[] searchDirectory(String path){
 		File f = new File(path);
-		return f.listFile();
+		return f.listFiles();
 	}
 	
-	boolean isFile(String path){
-		File f = new File(path);
+	boolean isFile(File f){
 		return !f.isDirectory();
 	}
 }
